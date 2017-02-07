62729e4d28a959f07e16dcd9a05c7125af2e0192
Andrew Throener <athroener@mystartupsolutions.com>
2017-02-06 20:40:30 -0600

    Quick Release Patch

--- routes/index.ts
+++ routes/index.ts
@@ -1,5 +1,9 @@
+import * as pack from '../package.json';
+
+const version = (<any>pack).version;
+
 export default function (router) {
     router.get('/', (req, res) => {
-        res.send('First Route! v2');
+        res.send('First Route! Version: ' + version);
     });
 }
