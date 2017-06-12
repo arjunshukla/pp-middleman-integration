f93c618e0db5c59e9d2b47d1c5d3f94e9ea531bc
Andrew Throener <athroener@mystartupsolutions.com>
2017-06-12 18:50:38 -0500

    Update

--- src/modules/intacct/classes/invoicing.ts
+++ src/modules/intacct/classes/invoicing.ts
@@ -40,7 +40,7 @@ export class IntacctInvoicingModule extends SubModule {
                 .getSubmodules()
                 .get('PayPalWebhooksModule')
                 .add(['INVOICING.INVOICE.CANCELLED', 'INVOICING.INVOICE.PAID', 'INVOICING.INVOICE.REFUNDED', 'INVOICING.INVOICE.UPDATED']),
-            this.query() // TODO: Need to create mongoos model and store the results of invoices in database.
+            this.startCron() 
         ]);
 
     }
@@ -49,6 +49,15 @@ export class IntacctInvoicingModule extends SubModule {
         const cid = intacct.IntacctApi.readByQuery({ object: 'ARINVOICE' });
         let invoices = await this.api.request(cid);
         return invoices.payload;
+    }
+
+    async startCron() {
+        let invoices = await this.query();
+        // TODO: Need to create mongoose model and store the results of invoices in database.
+        // TODO: Use setInterval to start the cronjob.  Use config from above.
+        // TODO: Should read webhooks from mongoose and do Intacct apis
+        console.log(invoices);
+        
     }
 
 }
